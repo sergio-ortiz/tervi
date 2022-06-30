4c4ab1c7d3e235adb72c6cf3113c4203b490e765
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-30 17:58:37 -0400

    feat(index): main heading

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
 
-const HomePage = () => <Layout>Hello World!</Layout>;
+const HomePage = () => (
+  <Layout>
+    <div className={styles.container}>
+      <h1 className={styles.mainHeading}>
+        Tervi strives to always be your 5-star rating Professional Janitorial
+        service provider.
+      </h1>
+    </div>
+  </Layout>
+);
 
 export default HomePage;
