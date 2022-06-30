7d5b1ea78ceb94f8e764ad4259d923f3d8d5119d
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-30 16:04:04 -0400

    feat(Hero): next/image component import inro header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
-import Nav from "/components/Nav";
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
+
 const Header = () => (
   <>
     <Nav />
+    <Hero />
   </>
 );
 
---
+++ components/Hero.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import homeHero from "../assets/home_hero.jpg";
+
+const Hero = () => <Image src={homeHero} height={1400} />;
+
+export default Hero;
