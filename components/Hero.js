afc36f6667e5fd96a6d6b26df1535a964b77846f
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-06-30 17:20:40 -0400

    refactor(Hero): mobile friendly styling and attributes on next/image component

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import homeHero from "../assets/home_hero.jpg";
+import styles from "../styles/Hero.module.css";
 
-const Hero = () => <Image src={homeHero} height={1400} />;
+const Hero = () => (
+  <div className={styles.hero}>
+    <Image
+      src={homeHero}
+      layout="fill"
+      objectFit="cover"
+      objectPosition="50% 60%"
+      placeholder="blur"
+      priority
+    />
+  </div>
+);
 
 export default Hero;
