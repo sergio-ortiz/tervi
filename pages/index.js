628f190a05035887011dc7dad9120a84835be731
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-07-01 14:34:42 -0400

    refactor(index): page component mainHeading text-content

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ const HomePage = () => (
   <Layout>
     <div className={styles.container}>
       <h1 className={styles.mainHeading}>
-        Tervi strives to always be your 5-star rating Professional Janitorial
-        service provider.
+        Tervi strives to be your 5-star rating Professional Janitorial service
+        provider.
       </h1>
       <ListBlock
         heading={"Our Work"}
