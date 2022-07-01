2cb3d3f30a60c65b434acb605382f108f0c61a3f
sergio-ortiz <ortiz.sergio0303@gmail.com>
2022-07-01 13:47:36 -0400

    feat(Footer): React component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Header from "/components/Header";
+import Footer from "/components/Footer";
 
 const Layout = ({ children }) => (
   <>
     <Header />
     {children}
+    <Footer />
   </>
 );
 
